98b5c5743895706156ba494361fde9779b5ada65
dd <devddstuff@gmail.com>
2018-02-21 19:14:19 -0500

    No optimizations by default in build scripts

--- build-clang-cl.cmd
+++ build-clang-cl.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 set t="\"%date% %time%\""
-clang-cl src\*.c src\executables\*.c src\etc\*.c src\images\*.c src\documents\*.c src\audio\*.c src\archives\*.c src\vdisk\*.c %1 -DTIMESTAMP=%t% /Zp /MD /GF- /GR- /Gd /GS- /Gw- /GX- /Gy- /Ox /o "ff.exe"
+clang-cl src\*.c src\executables\*.c src\etc\*.c src\images\*.c src\documents\*.c src\audio\*.c src\archives\*.c src\vdisk\*.c %1 -DTIMESTAMP=%t% /Zp /MD /o "ff.exe"
