8e7d7c38524fa276b8091f75033da7c2ce42a66b
dd <devddstuff@gmail.com>
2018-05-30 21:29:31 -0400

    header tweak

--- src/etc/ms-shllink.h
+++ src/etc/ms-shllink.h
@@ -3,9 +3,9 @@ struct ShellLinkHeader {
 	uint8_t clsid[16]; /// Class identifier. MUST be 00021401-0000-0000-C000-000000000046.
 	uint32_t flags; /// Link attributes
 	uint32_t attrs; /// File attributes
-	unsigned long long creation_time;
-	unsigned long long access_time;
-	unsigned long long write_time;
+	uint64_t creation_time;
+	uint64_t access_time;
+	uint64_t write_time;
 	uint32_t filesize;
 	uint32_t icon_index;
 	uint32_t show_command;
@@ -21,10 +21,10 @@ struct ShellLinkHeader {
 
 #define HasLinkTargetIDList 0x1 /// A
 #define HasLinkInfo 0x2 // B
-/*
+#define HasName 0x4 // C
+#define HasVolumeID 0x // 
 #define HasArguments 0x20 // F
 #define IsUnicode 0x80
 #define PreferEnvironmentPath 0x1000000 // Z
-*/
 
 void scan_lnk(void);
