a686da54434b7a0f04d972bc757c1bf320739904
dd86k <devddstuff@gmail.com>
2019-03-07 21:01:33 -0500

    Fix tar reading

--- src/archives/tar.h
+++ src/archives/tar.h
@@ -1,5 +1,10 @@
 #define TAR "ustar\000"
 #define GNUTAR "GNUtar\00"
+//TODO: 64-bit number versions with conditional compile option
+#define TAR32L 0x61747375
+#define TAR32H 0x00202072
+#define GNUTAR32L 0x74554E47
+#define GNUTAR32H 0x20007261
 #define NAMSIZ 100
 #define TUNMLEN 32
 #define TGNMLEN 32
--- src/etc.c
+++ src/etc.c
@@ -118,11 +118,18 @@ void scan_etc() {
 MOBI_OUT:
 
 	if (_ddseek(0x101, SEEK_SET)) goto _END; // Tar files
-	char btar[sizeof(TAR)];
-	_ddread(btar, sizeof(TAR));
-	if (strcmp(btar, TAR) == 0 || strcmp(btar, GNUTAR) == 0) {
-		scan_tar();
-		return;
+	//char btar[sizeof(TAR)];
+	//_ddread(btar, sizeof(TAR));
+	uint32_t btar32[2];
+	_ddread(btar32, 8);
+	//if (strcmp(btar, TAR) == 0 || strncmp(btar, GNUTAR, 8) == 0) {
+	switch (btar32[0]) {
+	case TAR32L: case GNUTAR32L:
+		switch (btar32[1]) {
+		case TAR32H: case GNUTAR32H:
+			scan_tar(); return;
+		}
+		break;
 	}
 
 	if (_ddseek(0x8001, SEEK_SET)) goto _END; // ISO files
